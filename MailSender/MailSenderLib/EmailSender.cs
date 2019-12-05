2fd37398c7c28c9f427d27528e7b6ec0e6bd33ce
Arhio <Arhio@yandex.ru>
2019-12-05 20:01:00 +0300

    C# - Уровень 3. Урок 1. Начало работы.

---
+++ MailSender/MailSenderLib/EmailSender.cs
@@ -0,0 +1,40 @@
+﻿using System.Net;
+using System.Net.Mail;
+
+namespace MailSenderLib
+{
+	/// <summary>
+	/// 
+	/// </summary>
+    public sealed class EmailSender
+    {
+		/// <summary>
+		/// Метод для отправки
+		/// </summary>
+		/// <param name="strUser"></param>
+		/// <param name="strPass"></param>
+		/// <exception cref="SmtpException"></exception>
+		public void Send(string strUser, string strPass)
+	    {
+			const string from = "user@yandex.ru";
+		    const string to = "user@gmail.com";
+
+		    const string server = "smtp.yandex.ru";
+
+		    try
+		    {
+			    using (var message = new MailMessage(from, to, "Test message", "Test messae body"))
+			    using (var client = new SmtpClient(server) { EnableSsl = true,
+				    Credentials = new NetworkCredential(strUser, strPass) })
+			    {
+				    client.Send(message);
+			    }
+
+			}
+		    catch (SmtpException error)
+		    {
+			   throw new SmtpException(error.Message);
+		    }
+		}
+	}
+}
